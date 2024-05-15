d48080324f33f6096cba9be6ceba27bc25bf64fb
AmirAbdullin <amirabdullin097@gmail.com>
2024-05-15 05:10:20 +0300

    3

--- cars.cpp
+++ cars.cpp
@@ -4,7 +4,14 @@
 
 #include <Qt>
 
-//Меню пользователя
+/**
+ * @brief Конструктор класса cars.
+ *
+ * Инициализирует новый объект класса cars с заданным родительским виджетом.
+ * Устанавливает размеры окна, конфигурирует интерфейс пользователя и подготавливает модели данных для работы с базой данных.
+ *
+ * @param parent Указатель на родительский виджет.
+ */
 cars::cars(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::cars)
@@ -17,7 +24,7 @@ cars::cars(QWidget *parent) :
 
     query = new QSqlQuery(db);
 
-    query->exec("CREATE TABLE Maint (ГРЗ TEXT NOT NULL, Работы    TEXT NOT NULL, Стоимость	TEXT NOT NULL, Комментарий TEXT NOT NULL, Дата	TEXT NOT NULL, Пробег	TEXT);");
+    query->exec("CREATE TABLE Maint (ГРЗ TEXT NOT NULL, Работы TEXT NOT NULL, Стоимость TEXT NOT NULL, Комментарий TEXT NOT NULL, Дата TEXT NOT NULL, Пробег TEXT);");
 
     model = new QSqlTableModel(this, db);
     model->setTable("Cars");
@@ -35,38 +42,55 @@ cars::cars(QWidget *parent) :
 
     querymodel = new QSqlQueryModel;
 
-    //Блокируем кнопки
+    // Блокировка кнопок до выбора элемента в таблице
     ui->pushButton->setEnabled(false);
     ui->repairsListButton->setEnabled(false);
 }
 
+/**
+ * @brief Деструктор класса cars.
+ *
+ * Освобождает все ресурсы, связанные с объектом и его элементами интерфейса.
+ */
 cars::~cars()
 {
     delete ui;
 }
 
-//Выход
+/**
+ * @brief Слот, реагирующий на нажатие кнопки выхода.
+ *
+ * Скрывает окно, эффективно закрывая его без завершения приложения.
+ */
 void cars::on_pushButton_3_clicked()
 {
     hide();
 }
 
-//Окно создания новго ТО
+/**
+ * @brief Слот для создания нового технического обслуживания (ТО).
+ *
+ * Запускает диалоговое окно для создания нового ТО для выбранного автомобиля.
+ * ГРЗ выбранного автомобиля передаётся в новое окно для дальнейшего использования.
+ */
 void cars::on_pushButton_clicked()
-{    
+{
     QModelIndex currentIndex = ui->carsTable->currentIndex();
     QString grz = (ui->carsTable->model()->data(ui->carsTable->model()->index(currentIndex.row(),0), 0)).toString();
 
     hide();
 
     newto to;
-    to.setGrz(grz); //Записывает Гос номер выбранного авто в новом окне
+    to.setGrz(grz); // Устанавливает Гос номер выбранного авто в новом окне
     to.setModal(true);
     to.exec();
-
 }
 
-//Открывает список всех ТО выьранного автомобиля
+/**
+ * @brief Слот для отображения списка всех технических обслуживаний выбранного автомобиля.
+ *
+ * Выполняет запрос к базе данных для получения списка ТО выбранного автомобиля и обновляет модель данных для отображения.
+ */
 void cars::on_repairsListButton_clicked()
 {
     QModelIndex currentIndex = ui->carsTable->currentIndex();
@@ -83,10 +107,13 @@ void cars::on_repairsListButton_clicked()
     ui->oldRepairsTable->setModel(querymodel);
 }
 
-
-
-
-//Разблокируем кнопки
+/**
+ * @brief Слот, активирующий кнопки управления.
+ *
+ * Разблокирует кнопки управления при выборе строки в таблице автомобилей.
+ *
+ * @param index Индекс выбранной строки в таблице.
+ */
 void cars::on_carsTable_clicked(const QModelIndex &index)
 {
     ui->pushButton->setEnabled(true);
